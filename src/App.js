c87d693c2862edebbc3b582b687670505cc8304d
elmar <elmarcera@gmail.com>
2024-03-05 17:33:30 +0800

    CHALLENGE #1: Fix Performance Issues in Workout Timer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Calculator from "./Calculator";
 import ToggleSounds from "./ToggleSounds";
+
+function formatTime(date) {
+  return new Intl.DateTimeFormat("en", {
+    month: "short",
+    year: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).format(date);
+}
 
 function App() {
   const [allowSound, setAllowSound] = useState(true);
@@ -9,38 +19,60 @@ function App() {
   // Will be be AM or PM
   const partOfDay = time.slice(-2);
 
-  const workouts = [
-    {
-      name: "Full-body workout",
-      numExercises: partOfDay === "AM" ? 9 : 8,
-    },
-    {
-      name: "Arms + Legs",
-      numExercises: 6,
-    },
-    {
-      name: "Arms only",
-      numExercises: 3,
-    },
-    {
-      name: "Legs only",
-      numExercises: 4,
-    },
-    {
-      name: "Core only",
-      numExercises: partOfDay === "AM" ? 5 : 4,
-    },
-  ];
+  // So since this is an array,
+  // which is essentially just a JavaScript object
+  // this array gets recreated on every single render.
+  // And so therefore we now need to memorize this array
+  // in order for the memorizing of the component to work.
+  // So let's do that.
+  const workouts = useMemo(() => {
+    return [
+      {
+        name: "Full-body workout",
+        numExercises: partOfDay === "AM" ? 9 : 8,
+      },
+      {
+        name: "Arms + Legs",
+        numExercises: 6,
+      },
+      {
+        name: "Arms only",
+        numExercises: 3,
+      },
+      {
+        name: "Legs only",
+        numExercises: 4,
+      },
+      {
+        name: "Core only",
+        numExercises: partOfDay === "AM" ? 5 : 4,
+      },
+    ];
+  }, [partOfDay]);
 
-  function formatTime(date) {
-    return new Intl.DateTimeFormat("en", {
-      month: "short",
-      year: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    }).format(date);
-  }
+  /////////////////////////////////////
+  //   Now there's just another small thing that we can do
+  // which is about this function here.
+  // So this function that is inside
+  // the component body right here,
+  // doesn't actually use any reactive value, right?
+  // And so there's no need to recreate this function
+  // on every render.
+  // And so let's cut that and place it out here.
+  // So another very small optimization
+  // but yeah, we don't need to waste any resources
+  // only to recreate this function when we don't need to to.
+
+  // function formatTime(date) {
+  //   return new Intl.DateTimeFormat("en", {
+  //     month: "short",
+  //     year: "2-digit",
+  //     hour: "2-digit",
+  //     minute: "2-digit",
+  //     second: "2-digit",
+  //   }).format(date);
+  // }
+  ///////////////////////////
 
   useEffect(function () {
     const id = setInterval(function () {
@@ -54,7 +86,32 @@ function App() {
     <main>
       <h1>Workout timer</h1>
       <time>For your workout on {time}</time>
+      So let's see what we can do.
+      {/* So remember that the memo function only works
+      if the props that are being passed
+      are the same between renders.
+      So here in this toggle sounds component,
+      that is apparently the case.
+      So allowSound is just a primitive value
+      so it's just a boolean
+      and so therefore that doesn't change across renders.
+      And then the second prop is this setAllowSound function.
+      Now remember that the state setter function
+      from useState is actually stable.
+      So React guarantees that it doesn't change between renders.
+      And so this means
+      that these two props do not change between renders
+      and so therefore the toggle sounds component
+      does not re-render each time that the app re-renders.
+      So we successfully memoed or memorized this one.
+      Now about the calculator component.
+      The allowSounds of course also stays stable.
+       */}
       <ToggleSounds allowSound={allowSound} setAllowSound={setAllowSound} />
+      {/* ///////////////// */}
+      {/* But now the other prop is the workouts object
+      or actually the array.
+      And so here is where we have the problem. */}
       <Calculator workouts={workouts} allowSound={allowSound} />
     </main>
   );
--- src/ToggleSounds.js
+++ src/ToggleSounds.js
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 function ToggleSounds({ allowSound, setAllowSound }) {
   return (
     <button
@@ -9,4 +11,17 @@ function ToggleSounds({ allowSound, setAllowSound }) {
   );
 }
 
-export default ToggleSounds;
+// Now remember how earlier we memorized a component
+// by basically creating a new variable here,
+// like toggle sounds
+// and then we would wrap this function into memo.
+// However since here,
+// we are actually exporting these components,
+// It's a lot easier and nicer to just memorize down here.
+// So let me show you what I mean by that.
+// So we can just export basically the results
+// of memorizing this component.
+// And so this again, is a lot better
+// than creating a new separate variable here
+// which makes the code look a little bit weird.
+export default memo(ToggleSounds);
